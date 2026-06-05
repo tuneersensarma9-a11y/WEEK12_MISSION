d67a16896e5df86517b2cf0eb4ce38455fdd03ff
tuneersensarma9-a11y <tuneersensarma9@gmail.com>
2026-06-05 23:44:23 +0530

    Update server.js

--- websocket-room/backend/server.js
+++ websocket-room/backend/server.js
@@ -11,29 +11,31 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
-    methods: ["GET", "POST"],
-  },
+  origin: [
+    "http://localhost:5173",
+    "https://your-vercel-app.vercel.app"
+  ],
+  methods: ["GET", "POST"],
+},
 });
 
 io.on("connection", (socket) => {
   console.log(`Client Connected: ${socket.id}`);
 
-  // Receive message from client
+ 
   socket.on("send_message", (data) => {
     console.log("Message Received:", data);
 
-    // Send message to all connected clients
+    
     io.emit("receive_message", data);
   });
 
-  // User started typing
+  
 socket.on("typing", () => {
   console.log("Typing event received");
   socket.broadcast.emit("user_typing");
 });
 
-// User stopped typing
 socket.on("stop_typing", () => {
   console.log("Stop typing event received");
   socket.broadcast.emit("user_stopped_typing");
