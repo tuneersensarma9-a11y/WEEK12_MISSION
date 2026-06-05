73b8cfb22e4d1df07c6d6c25cb6dd96bb3b7cd90
tuneersensarma9-a11y <tuneersensarma9@gmail.com>
2026-06-06 00:12:48 +0530

    Update App.jsx

--- websocket-room/frontend/src/App.jsx
+++ websocket-room/frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { io } from "socket.io-client";
 import "./App.css";
 
-const socket = io("https://week12-mission.onrender.com/");
+const socket = io("https://week12-mission.onrender.com");
 
 function App() {
   const [message, setMessage] = useState("");
